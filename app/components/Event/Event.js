d77be5e1489dbf694276cbd4335e653ea53d2ff9
Baboo7 <baptiste.studer@laposte.net>
2016-12-19 22:43:17 +0100

    modification of the component Event add of a folder icons in the images folder

--- app/components/Event/Event.js
+++ app/components/Event/Event.js
@@ -1,29 +1,50 @@
 import React, { Component } from 'react';
 import {
   Text,
-  View
+  View,
+  Image,
 } from 'react-native';
 
 import styles from './styles';
 
+/*
+	Props:
+	- product: id of the product
+	- producer: id of the producer
+	- daysLeft: number of days left before the event ends
+*/
+
 class Event extends Component {
-  render() {
-    return (
-      <View>
-        <Text style={styles.product}>
-          {this.props.product}
-        </Text>
-        <View style={styles.subtitle}>
-          <Text style={styles.producer}>
-            {this.props.producer}
-          </Text>
-          <Text style={styles.daysLeft}>
-            {this.props.daysLeft}
-          </Text>
-        </View>
-      </View>
-    );
-  }
+	render() {
+		return (
+			// Main container
+			<View style={styles.mainContainer}>
+				// Image container
+				<View style={styles.imageContainer}>
+					<Image
+						source={require('../../images/icons/radish.png')}
+					/>
+				</View>
+
+				// Text container
+				<View style={styles.textContainer}>
+					<Text style={styles.product}>
+						{this.props.product}
+					</Text>
+
+					<View style={styles.subtitle}>
+						<Text style={styles.producer}>
+							{this.props.producer}
+						</Text>
+
+						<Text style={styles.daysLeft}>
+							{this.props.daysLeft}
+						</Text>
+					</View>
+				</View>
+			</View>
+		);
+	}
 }
 
 export default Event;
--- app/components/Event/styles.js
+++ app/components/Event/styles.js
@@ -1,21 +1,34 @@
 import { StyleSheet } from 'react-native';
 import colors from '../../config/colors'
+import metrics from '../../config/metrics'
 
 export default styles = StyleSheet.create({
-  daysLeft: {
-    fontSize: 20,
-    flex: 0.2
-  },
-  producer: {
-    fontSize: 20,
-    flex: 0.8
-  },
-  product: {
-    fontSize: 30,
-    fontWeight: 'bold'
-  },
-  subtitle:{
-    flex: 1,
-    flexDirection: 'row'
-  }
+	mainContainer: {
+		flex: 1,
+		flexDirection: 'row',
+		backgroundColor: colors.salmonLight,
+	},
+	imageContainer: {
+		marginRight: metrics.baseMargin,
+	},
+	textContainer: {
+		flex: 1,
+	},
+	daysLeft: {
+		fontSize: 20,
+		flex: 0.3,
+		textAlign: 'right',
+	},
+	producer: {
+		fontSize: 20,
+		flex: 0.7
+	},
+	product: {
+		fontSize: 30,
+		fontWeight: 'bold'
+	},
+	subtitle:{
+		flex: 1,
+		flexDirection: 'row'
+	},
 });
