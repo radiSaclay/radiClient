40fb6fbad8de953e6cad49e426941c8ee99cbdfe
Baboo7 <baptiste.studer@laposte.net>
2016-12-19 22:43:17 +0100

    modification of the component Event add of a folder icons in the images folder

--- app/config/metrics.js
+++ app/config/metrics.js
@@ -3,14 +3,14 @@ import {Dimensions} from 'react-native'
 const { width, height } = Dimensions.get('window')
 
 const metrics = {
-  borderRadius: 4,
-  baseMargin: 10,
-  basePadding: 10,
-  logoHeight: 70,
-  logoWidth: 60,
-  navBarHeight: 54,
-  screenHeight: width < height ? height : width,
-  screenWidth: width < height ? width : height,
+	borderRadius: 4,
+	baseMargin: 10,
+	basePadding: 10,
+	logoHeight: 70,
+	logoWidth: 60,
+	navBarHeight: 54,
+	screenHeight: width < height ? height : width,
+	screenWidth: width < height ? width : height,
 }
 
 export default metrics
--- app/index.js
+++ app/index.js
@@ -6,31 +6,33 @@ import EventDetail from './routes/EventDetail';
 import EventsList from './routes/EventsList';
 
 const App = () => {
-  return (
-    <Router>
-      <Scene key="root">
-        <Scene
-          component={SignIn}
-          hideNavBar={true}
-          initial={true}
-          key="SignIn"
-          title="Sign In"
-        />
-        <Scene
-          component={EventsList}
-          hideNavBar={false}
-          key="EventsList"
-          title="Events List"
-        />
-        <Scene
-          component={EventDetail}
-          hideNavBar={false}
-          key="EventDetail"
-          title="Détails de l'évènement"
-        />
-      </Scene>
-    </Router>
-  )
+	return (
+		<Router>
+			<Scene key="root">
+				<Scene
+					component={SignIn}
+					hideNavBar={true}
+					initial={true}
+					key="SignIn"
+					title="Sign In"
+				/>
+
+				<Scene
+					component={EventsList}
+					hideNavBar={false}
+					key="EventsList"
+					title="Events List"
+				/>
+				
+				<Scene
+					component={EventDetail}
+					hideNavBar={false}
+					key="EventDetail"
+					title="Détails de l'évènement"
+				/>
+			</Scene>
+		</Router>
+	)
 }
 
 export default App;
