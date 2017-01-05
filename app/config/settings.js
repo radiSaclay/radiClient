a0576f4b4ce8375285806c8abb3d0df84c571727
febeck <fe.beck25@gmail.com>
2017-01-05 16:25:51 +0100

    Sign up user

---
+++ app/config/settings.js
@@ -0,0 +1,5 @@
+const settings = {
+  AUTH_URL: 'http://147.250.84.250:3001/radisaclay/public/auth/'
+}
+
+export default settings
--- app/routes/SignIn/SignIn.js
+++ app/routes/SignIn/SignIn.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
 import {
-	Image,
-	Text,
-	TextInput,
-	TouchableOpacity,
-	View
+  Alert,
+  AsyncStorage,
+  Image,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View
 } from 'react-native';
 import {
 	ActionConst,
@@ -12,57 +14,117 @@ import {
 } from 'react-native-router-flux';
 
 import styles from './styles';
+import settings from '../../config/settings'
+
+var STORAGE_KEY = 'id_token'
 
 class SignIn extends Component {
-	render() {
-		return (
-			<View style={styles.container}>
-				<Image
-					source={require('../../images/logo.png')}
-					style={styles.logo}
-				/>
+  constructor(){
+    super();
+    this.state = {
+      email: null,
+      password: null
+    }
+  }
 
-				<Text style={styles.title}>
-					RadiSaclay
-				</Text>
+  async onValueChange(item, selectedValue) {
+      try {
+        await AsyncStorage.setItem(item, selectedValue);
+      } catch (error) {
+        console.log('AsyncStorage error: ' + error.message);
+      }
+  }
 
-				<View style={styles.form}>
-					<TextInput
-						style={styles.inputText}
-						editable={true}
-						keyboardType='email-address'
-						returnKeyType='next'
-						autoCapitalize='none'
-						autoCorrect={false}
-						underlineColorAndroid='transparent'
-						placeholder='Username'
-					/>
-					<TextInput
-						style={styles.inputText}
-						editable={true}
-						keyboardType='default'
-						returnKeyType='next'
-						autoCapitalize='none'
-						autoCorrect={false}
-						underlineColorAndroid='transparent'
-						secureTextEntry={true}
-						placeholder='Password'
-					/>
+  userSignup() {
+    if (this.state.email && this.state.password){
+      fetch(settings.AUTH_URL + "signup", {
+        method: "POST",
+        headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+          email: this.state.email,
+          password: this.state.password,
+        })
+      })
+      .then((response) => response.json())
+      .then((responseData) => {
+        if (responseData.validated){
+          this.onValueChange(STORAGE_KEY, responseData.token),
+					Actions.MainTab({type: ActionConst.REPLACE})
+        }
+        else{
+          Alert.alert(
+            "Inscription pas valide",
+            responseData.msg
+          )
+        }
+      })
+      .done();
+    }
+  }
 
-					<TouchableOpacity style={styles.loginButtonWrapper}>
-						<Text
-							style={styles.loginText}
+  render() {
+    return (
+      <View style={styles.container}>
+        <Image
+          source={require('../../images/logo.png')}
+          style={styles.logo}
+        />
+        <Text style={styles.title}>
+          RadiSaclay
+        </Text>
+        <View style={styles.form}>
+          <TextInput
+            autoCapitalize='none'
+            autoCorrect={false}
+            editable={true}
+            keyboardType='email-address'
+            onChangeText={(email) => this.setState({email})}
+            placeholder='Email'
+            ref='email'
+            returnKeyType='next'
+            style={styles.inputText}
+            value={this.state.email}
+            underlineColorAndroid='transparent'
+          />
+          <TextInput
+            autoCapitalize='none'
+            autoCorrect={false}
+            editable={true}
+            keyboardType='default'
+            onChangeText={(password) => this.setState({password})}
+            placeholder='Password'
+            ref='password'
+            returnKeyType='next'
+            secureTextEntry={true}
+            style={styles.inputText}
+            value={this.state.password}
+            underlineColorAndroid='transparent'
+          />
+          <TouchableOpacity style={styles.buttonWrapper}>
+            <Text
+              style={styles.buttonText}
 							onPress={() => {
 								Actions.MainTab({type: ActionConst.REPLACE})
 							}}
-						>
-							Log In
-						</Text>
-					</TouchableOpacity>
-				</View>
-			</View>
-		);
-	}
+            >
+              Log In
+            </Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.buttonWrapper}>
+            <Text
+              style={styles.buttonText}
+              onPress={this.userSignup.bind(this)}
+            >
+              Sign Up
+            </Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+    );
+  }
 }
 
 export default SignIn;
