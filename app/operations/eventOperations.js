d30dec3e5ed9c1c21f7de8ad72a46c06f0f564e3
febeck <fe.beck25@gmail.com>
2017-02-01 10:13:12 +0100

    Create and test event operations

---
+++ app/operations/eventOperations.js
@@ -0,0 +1,16 @@
+import * as actions from '../actions/eventActions'
+import promises from '../config/promises'
+import settings from '../config/settings'
+
+import axios from 'axios'
+
+export function eventsListFetch (idToken) {
+	return dispatch => {
+		dispatch(actions.eventsListFetchRequest())
+		return promises.getWithToken(settings.urls.EVENTS_URL,idToken)
+		.then((events) => {
+			dispatch(actions.eventsListFetchSuccess(events.data))
+		})
+		.catch(error => dispatch(actions.eventsListFetchError(error)))
+	}
+}
