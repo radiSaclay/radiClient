c4b0f285ba6043095fb924b0c3e3c156de9918e2
febeck <fe.beck25@gmail.com>
2017-02-02 12:52:42 +0100

    Persist redux store on the AsyncStorage

--- app/configureStore.js
+++ app/configureStore.js
@@ -1,11 +1,36 @@
 import { applyMiddleware, createStore } from 'redux'
 import logger from 'redux-logger'
+import * as storage from 'redux-storage';
+import createEngine from 'redux-storage-engine-reactnativeasyncstorage';
 import thunkMiddleware from 'redux-thunk'
 
 import rootReducer from './reducers'
 
-const middleware = applyMiddleware(logger(), thunkMiddleware)
+const middleWare = [];
+
+// Add thunk middleWare
+middleWare.push(thunkMiddleware);
+
+// Add redux store storage reducer
+const wrappedReducer = storage.reducer(rootReducer);
+let engine = createEngine('radiSaclay');
+// TODO: debounce
+// TODO: filter
+const reduxStorageMiddleware = storage.createMiddleware(engine);
+middleWare.push(reduxStorageMiddleware);
+const loadStore = storage.createLoader(engine);
+
+
+// Add logger middleWare. This has always to be the last
+middleWare.push(logger())
+
+
+const createStoreWithMiddleware = applyMiddleware(...middleWare)(createStore);
 
 export default function configureStore() {
-	return createStore(rootReducer, middleware)
+	const store = createStoreWithMiddleware(wrappedReducer);
+	loadStore(store)
+	.then((newState) => {})
+	.catch((error) => { console.log('Failed to load previous store'); })
+	return store;
 }
