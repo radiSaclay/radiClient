2cbab420f1a769b803936cb462f1e207e1ce5fd2
febeck <fe.beck25@gmail.com>
2017-02-02 15:12:59 +0100

    Throttle redux store saving operation

--- app/configureStore.js
+++ app/configureStore.js
@@ -1,6 +1,7 @@
 import { applyMiddleware, createStore } from 'redux'
 import logger from 'redux-logger'
 import * as storage from 'redux-storage';
+import debounce from 'redux-storage-decorator-debounce';
 import createEngine from 'redux-storage-engine-reactnativeasyncstorage';
 import thunkMiddleware from 'redux-thunk'
 
@@ -14,8 +15,9 @@ middleWare.push(thunkMiddleware);
 // Add redux store storage reducer
 const wrappedReducer = storage.reducer(rootReducer);
 let engine = createEngine('radiSaclay');
-// TODO: debounce
-// TODO: filter
+// Throttle save operation to every once a second at max
+engine = debounce(engine, 1000);
+// TODO: parameter filter to save only parts of the store
 const reduxStorageMiddleware = storage.createMiddleware(engine);
 middleWare.push(reduxStorageMiddleware);
 const loadStore = storage.createLoader(engine);
