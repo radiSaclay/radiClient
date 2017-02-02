d571882549922fe64b97a83292cfa5c9fcf14638
Baboo7 <baptiste.studer@laposte.net>
2017-02-02 18:27:04 +0100

    Create ProductDetail route

---
+++ app/routes/ProductDetail/styles.js
@@ -0,0 +1,22 @@
+import { StyleSheet } from 'react-native'
+import colors from '../../config/colors'
+import metrics from '../../config/metrics'
+
+export default styles = StyleSheet.create({
+	buttonText: {
+		fontSize: 20,
+		padding: metrics.basePadding,
+		textAlign: 'center',
+	},
+	buttonWrapper: {
+		backgroundColor: colors.coral,
+		borderRadius: metrics.borderRadius,
+	},
+	container: {
+		top: metrics.navBarHeight,
+	},
+	name: {
+		fontSize: 30,
+		fontWeight: 'bold',
+	},
+})
