ebac058bb80476eeb6abfb1a657675bf768505d4
febeck <fe.beck25@gmail.com>
2017-02-08 20:12:55 +0100

    Create news operation and update NewsContainer component

---
+++ app/operations/newsOperations.js
@@ -0,0 +1,23 @@
+import configureStore from '../configureStore'
+
+import * as actions from '../actions/newsActions'
+import * as eventOperations from './eventOperations'
+import * as farmOperations from './farmOperations'
+import * as productOperations from './productOperations'
+
+const store = configureStore()
+
+export function newsFetch (idToken) {
+	return dispatch => {
+		dispatch(actions.newsFetchRequest())
+		return Promise.all([
+			dispatch(eventOperations.eventsListFetch(idToken)),
+			dispatch(farmOperations.farmsListFetch(idToken)),
+			dispatch(productOperations.productsListFetch(idToken)),
+		])
+		.then(() => {
+			dispatch(actions.newsFetchSuccess())
+		})
+		.catch(error => dispatch(actions.newsFetchError(error)))
+	}
+}
