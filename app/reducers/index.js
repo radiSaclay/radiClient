224f2b831f5b2263b1a0f761ed19a74a612b8cc9
febeck <fe.beck25@gmail.com>
2017-02-08 20:11:28 +0100

    Create and test news reducer

--- app/reducers/index.js
+++ app/reducers/index.js
@@ -1,12 +1,14 @@
 import { combineReducers } from "redux"
 import events from "./eventReducer"
 import farms from "./farmReducer"
+import news from "./newsReducer"
 import products from "./productReducer"
 import user from "./userReducer"
 
 export default rootReducer = combineReducers({
 	events,
 	farms,
+	news,
 	products,
 	user
 })
