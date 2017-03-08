fd2ee18685ee0cf3e165df094a88790a07fea96a
Baboo7 <baptiste.studer@laposte.net>
2017-03-08 11:34:20 +0100

    Clean code in Frame/styles.js

--- app/components/Frame/styles.js
+++ app/components/Frame/styles.js
@@ -1,6 +1,4 @@
 import { StyleSheet } from 'react-native';
-import colors from '../../config/colors';
-import metrics from '../../config/metrics';
 
 export default styles = StyleSheet.create({
 	bodyContainer: {
