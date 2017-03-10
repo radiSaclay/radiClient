f964e82c1f98a7e2924eac6eab3dec2aaca6c984
febeck <fe.beck25@gmail.com>
2017-03-10 11:29:46 +0100

    Remove old user reducer properties and update tests

--- __tests__/reducers/userReducerTest.js
+++ __tests__/reducers/userReducerTest.js
@@ -6,44 +6,17 @@ import actionTypes from '../../app/config/actionTypes';
 describe('userReducer', () =>{
 	it('should return the initialState', () => {
 		let expectedState = {
-			error: null,
 			idToken: null,
-			isLoading: false,
-			isMounted: false
 		}
 		expect(userReducer(undefined, {})).toEqual(expectedState)
-	})
-
-	it('should handle REDUX_STORAGE_LOAD', () => {
-		let expectedState = {
-			isMounted: true
-		}
-		expect(userReducer([], { type: actionTypes.REDUX_STORAGE_LOAD })).toEqual(expectedState)
-	})
-
-	it('should handle USER_AUTHENTICATION_REQUEST', () => {
-		let expectedState = {
-			isLoading: true,
-		}
-		expect(userReducer([], userActions.authRequest())).toEqual(expectedState)
 	})
 
 	it('should handle USER_AUTHENTICATION_SUCCESS', () => {
 		let idToken = 'dbnub23dunb23p9ub238b'
 		let expectedState = {
 			idToken: idToken,
-			isLoading: false,
 		}
 		expect(userReducer([], userActions.authSuccess(idToken))).toEqual(expectedState)
-	})
-
-	it('should handle USER_AUTHENTICATION_ERROR', () => {
-		let error = new Error()
-		let expectedState = {
-			error: error,
-			isLoading: false,
-		}
-		expect(userReducer([], userActions.authError(error))).toEqual(expectedState)
 	})
 
 	it('should handle USER_LOGOUT', () => {
