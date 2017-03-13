9e4339a7f71ae888ff4649797ef3534f5ede5313
febeck <fe.beck25@gmail.com>
2017-03-13 18:25:43 +0100

    Install OneSignal SDK and link to project

--- android/app/src/main/java/com/radisaclay/MainApplication.java
+++ android/app/src/main/java/com/radisaclay/MainApplication.java
@@ -4,6 +4,7 @@ import android.app.Application;
 import android.util.Log;
 
 import com.facebook.react.ReactApplication;
+import com.geektime.rnonesignalandroid.ReactNativeOneSignalPackage;
 import com.facebook.react.ReactInstanceManager;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
@@ -36,6 +37,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
         new MainReactPackage(),
+            new ReactNativeOneSignalPackage(),
         new FBSDKPackage(mCallbackManager)
       );
     }
