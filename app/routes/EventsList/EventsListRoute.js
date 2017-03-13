9b1bf97798cf403c822ee183cece0bba5aa75380
Baboo7 <baptiste.studer@laposte.net>
2017-03-13 16:44:36 +0100

    Fix style for EventsListRoute

--- app/routes/EventsList/EventsListRoute.js
+++ app/routes/EventsList/EventsListRoute.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
+import { View } from 'react-native';
 
 import settings from '../../config/settings';
+import styles from './styles';
 
 import EventsListContainer from './EventsListContainer';
 import Frame from '../../components/Frame';
@@ -9,7 +11,9 @@ class EventsListRoute extends Component {
 	render() {
 		return (
 			<Frame title={this.props.title}>
-				<EventsListContainer/>
+				<View style={styles.route}>
+					<EventsListContainer/>
+				</View>
 			</Frame>
 		)
 	}
