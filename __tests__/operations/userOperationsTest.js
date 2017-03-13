8012081eb6ec2e993191659cb81c439f13d1008c
febeck <fe.beck25@gmail.com>
2017-03-13 15:30:03 +0100

    Add test for facebook auth user operation

--- __tests__/operations/userOperationsTest.js
+++ __tests__/operations/userOperationsTest.js
@@ -9,7 +9,7 @@ import settings from '../../app/config/settings'
 const middlewares = [ thunk ]
 const mockStore = configureMockStore(middlewares)
 
-describe('User authentication operations', () => {
+describe('User authentication operations with regular email and password', () => {
 
 	const idToken = 'bqdiuqbw9udb19b129dub1'
 	const requestBody = {email: 'abcd@xyz.com', password: 'password'}
@@ -52,6 +52,54 @@ describe('User authentication operations', () => {
 			.reply(errorStatus, {msg: errorMessage})
 
 		return store.dispatch(operations.userAuth(url, requestBody))
+			.then(() => {
+				expect(store.getActions()).toEqual(expectedActions)
+			})
+	})
+})
+
+describe('User authentication operations with facebook', () => {
+
+	const facebookToken = 'bqdiuqbw9udb19b129dub1'
+	const idToken = 'bqdiuqbw9udb19b129dub1'
+	const requestHeaders = {'Authorization': facebookToken}
+	const url = settings.urls.AUTH_FACEBOOK_URL
+
+	afterEach(() => {
+		nock.cleanAll()
+	})
+
+	it('creates USER_AUTHENTICATION_SUCCESS when authenticating user is done', () => {
+		let store = mockStore({idToken: null})
+		let expectedActions = [
+			userActions.authRequest(),
+			userActions.authSuccess(idToken)
+		]
+
+		nock(url, requestHeaders)
+			.post('')
+			.reply(200, { token: idToken })
+
+		return store.dispatch(operations.userFacebookAuth(url, facebookToken))
+			.then(() => {
+				expect(store.getActions()).toEqual(expectedActions)
+			})
+	})
+
+	it('creates USER_AUTHENTICATION_ERROR when authenticating user returns an error', () => {
+		let errorMessage = 'something awful happened'
+		let errorStatus = 401
+		let expectedActions = [
+			userActions.authRequest(),
+			userActions.authError(errorMessage, errorStatus)
+		]
+		let store = mockStore({idToken: null})
+
+		nock(url, requestHeaders)
+			.post('')
+			.reply(errorStatus, {msg: errorMessage})
+
+		return store.dispatch(operations.userFacebookAuth(url, facebookToken))
 			.then(() => {
 				expect(store.getActions()).toEqual(expectedActions)
 			})
